6c27c4a4ae4ff57272a4ee96334205a91401c2ba
Reprieving <shopping4lee@gmail.com>
2023-01-04 23:29:38 +0800

    ~

---
+++ src/main/java/com/byritium/conn/BootNettyServer.java
@@ -0,0 +1,24 @@
+package com.byritium.conn;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.net.InetSocketAddress;
+
+public class BootNettyServer {
+
+    public void startup(){
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup work = new NioEventLoopGroup();
+        bootstrap.group(boss,work)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                .channel(NioServerSocketChannel.class);
+
+    }
+}
