e4f471201e94c4ee998df03ef5a212e6d7b0661f
Reprieving <shopping4lee@gmail.com>
2023-01-11 22:15:06 +0800

    ~

--- src/main/java/com/byritium/conn/BootNettyServer.java
+++ src/main/java/com/byritium/conn/BootNettyServer.java
@@ -1,6 +1,9 @@
 package com.byritium.conn;
 
 import com.byritium.conn.domain.protocol.base.NettyServerInitializer;
+import com.byritium.conn.domain.protocol.base.ProtocolFactory;
+import com.byritium.conn.domain.protocol.base.ProtocolHandler;
+import com.byritium.conn.infrastructure.utils.SpringUtils;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
@@ -10,16 +13,26 @@ import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 
 public class BootNettyServer {
 
-    public void startup(){
+    public void startup() throws InterruptedException {
         ServerBootstrap bootstrap = new ServerBootstrap();
         EventLoopGroup boss = new NioEventLoopGroup();
         EventLoopGroup work = new NioEventLoopGroup();
-        bootstrap.group(boss,work)
+        bootstrap.group(boss, work)
                 .handler(new LoggingHandler(LogLevel.DEBUG))
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new NettyServerInitializer());
+
+        ProtocolFactory protocolFactory = SpringUtils.getBean(ProtocolFactory.class);
+        Map<Integer, List<ProtocolHandler>> map = protocolFactory.get();
+        for (Map.Entry<Integer, List<ProtocolHandler>> entry : map.entrySet()) {
+            int port = entry.getKey();
+            ChannelFuture f = bootstrap.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }
     }
 }
--- src/main/java/com/byritium/conn/domain/protocol/base/NettyServerInitializer.java
+++ src/main/java/com/byritium/conn/domain/protocol/base/NettyServerInitializer.java
@@ -3,6 +3,7 @@ package com.byritium.conn.domain.protocol.base;
 import com.byritium.conn.domain.protocol.http.HttpChannelHandler;
 import com.byritium.conn.domain.protocol.mqtt.MqttChannelHandler;
 import com.byritium.conn.infrastructure.utils.SpringUtils;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
@@ -10,17 +11,31 @@ import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.mqtt.MqttDecoder;
 import io.netty.handler.codec.mqtt.MqttEncoder;
+import org.springframework.util.StringUtils;
 
 import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
 
 public class NettyServerInitializer extends ChannelInitializer<SocketChannel> {
     @Override
-    protected void initChannel(SocketChannel socketChannel) throws Exception {
+    protected void initChannel(SocketChannel socketChannel) {
         ChannelPipeline pipeline = socketChannel.pipeline();
-
         InetSocketAddress socketAddress = socketChannel.localAddress();
         int port = socketAddress.getPort();
-
         ProtocolFactory protocolFactory = SpringUtils.getBean(ProtocolFactory.class);
+        List<ProtocolHandler> protocolHandlerList = protocolFactory.get(port);
+        for (ProtocolHandler protocolHandler : protocolHandlerList) {
+            String name = protocolHandler.getName();
+            ChannelHandler channelHandler = protocolHandler.getChannelHandler();
+            if (channelHandler == null) {
+                continue;
+            }
+            if (StringUtils.hasText(name)) {
+                pipeline.addLast(name, channelHandler);
+            } else {
+                pipeline.addLast(channelHandler);
+            }
+        }
     }
 }
