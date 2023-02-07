3376b8f3289cb138ac71627467e32f208e093a49
Reprieving <shopping4lee@gmail.com>
2023-02-07 23:37:43 +0800

    ~

---
+++ src/main/java/com/byritium/conn/apis/model/ConnectionInfo.java
@@ -0,0 +1,5 @@
+package com.byritium.conn.apis.model;
+
+public class ConnectionInfo {
+
+}
---
+++ src/main/java/com/byritium/conn/application/ConnectionAppService.java
@@ -0,0 +1,17 @@
+package com.byritium.conn.application;
+
+import com.byritium.conn.apis.model.ConnectionInfo;
+import io.netty.channel.ChannelId;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConnectionAppService {
+
+    public void connect(ChannelId channelId, ConnectionInfo connectionInfo){
+
+    }
+
+    public void disconnect(ChannelId channelId){
+
+    }
+}
