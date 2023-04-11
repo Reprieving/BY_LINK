afcc6a3f1da17ef78b38d549a410df108654805e
Reprieving <shopping4lee@gmail.com>
2023-04-11 23:23:13 +0800

    ~

---
+++ src/main/java/com/byritium/conn/domain/connection/service/ConnectionInfoService.java
@@ -0,0 +1,21 @@
+package com.byritium.conn.domain.connection.service;
+
+import com.byritium.conn.infra.peristent.respository.ConnectionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConnectionInfoService {
+
+    @Autowired
+    private ConnectionRepository connectionRepository;
+
+    public void pointMessage(){
+
+    }
+
+    public void borderCastMessage(){
+
+    }
+
+}
--- src/main/java/com/byritium/conn/infra/peristent/respository/ConnectionRepository.java
+++ src/main/java/com/byritium/conn/infra/peristent/respository/ConnectionRepository.java
@@ -6,21 +6,25 @@ import io.netty.channel.ChannelId;
 import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
+import org.springframework.stereotype.Repository;
 
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
+@Repository
 public class ConnectionRepository {
     private static final ChannelGroup globalGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     private static final ConcurrentMap<String, ChannelId> channelMap = new ConcurrentHashMap<>();
 
-    public static void connect(Channel channel){
+    public static void connect(String objectId, Channel channel) {
         globalGroup.add(channel);
-        channelMap.put(channel.id().asShortText(), channel.id());
+        channelMap.put(objectId, channel.id());
     }
 
-    public static void disconnect(Channel channel){
+    public static void disconnect(String objectId) {
+        ChannelId channelId = channelMap.get(objectId);
+        Channel channel = globalGroup.find(channelId);
         globalGroup.remove(channel);
-        channelMap.remove(channel.id().asShortText());
+        channelMap.remove(objectId);
     }
 }
