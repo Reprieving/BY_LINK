7166507d7a118899222eae66dd459b93ec36f95b
Reprieving <shopping4lee@gmail.com>
2023-04-12 23:16:18 +0800

    ~

--- src/main/java/com/byritium/conn/domain/connection/service/ConnectionInfoService.java
+++ src/main/java/com/byritium/conn/domain/connection/service/ConnectionInfoService.java
@@ -1,14 +1,30 @@
 package com.byritium.conn.domain.connection.service;
 
-import com.byritium.conn.infra.peristent.respository.ConnectionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelId;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.springframework.stereotype.Service;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 @Service
 public class ConnectionInfoService {
+    private static final ChannelGroup globalGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+    private static final ConcurrentMap<String, ChannelId> channelMap = new ConcurrentHashMap<>();
 
-    @Autowired
-    private ConnectionRepository connectionRepository;
+    public void connect(String objectId, Channel channel) {
+        globalGroup.add(channel);
+        channelMap.put(objectId, channel.id());
+    }
+
+    public void disconnect(String objectId) {
+        ChannelId channelId = channelMap.get(objectId);
+        Channel channel = globalGroup.find(channelId);
+        globalGroup.remove(channel);
+        channelMap.remove(objectId);
+    }
 
     public void pointMessage(){
 
