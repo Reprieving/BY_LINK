ad9054fd9ab83f585f34e1993edbe3547e8fdb8e
Reprieving <shopping4lee@gmail.com>
2023-04-16 23:57:40 +0800

    ~

--- src/main/java/com/byritium/conn/domain/connection/manager/ConnectionProcessor.java
+++ src/main/java/com/byritium/conn/domain/connection/manager/ConnectionProcessor.java
@@ -1,12 +1,13 @@
 package com.byritium.conn.domain.connection.manager;
 
 import com.byritium.conn.infra.general.constance.ProtocolType;
+import io.netty.channel.Channel;
 
 public interface ConnectionProcessor {
 
     ProtocolType protocolType();
 
-    void connect();
+    void connect(Channel channel, Object message);
 
-    void messaged();
+    void messaged(Channel channel,Object message);
 }
