50437770188d06f82e7c7a71f2e7a2d48d1b08ef
Reprieving <shopping4lee@gmail.com>
2023-08-09 14:16:15 +0800

    ~

--- src/main/java/com/byritium/apis/http/AccountController.java
+++ src/main/java/com/byritium/apis/http/AccountController.java
@@ -1,10 +1,26 @@
 package com.byritium.apis.http;
 
+import com.byritium.application.AccountAppService;
+import com.byritium.application.command.AccountCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("account")
 public class AccountController {
+
+    @Autowired
+    private AccountAppService accountAppService;
+
+    @RequestMapping("register")
+    public void register(AccountCommand accountCommand) {
+        accountAppService.resisterAccount(accountCommand);
+    }
+
+    @RequestMapping("logout")
+    public void logout() {
+
+    }
 
 }
