2b86509dd2bdb193414790014963ed36967de0e8
Reprieving <shopping4lee@gmail.com>
2023-08-21 10:10:31 +0800

    ~

--- src/main/java/com/byritium/persistence/convertor/GroupConvertor.java
+++ src/main/java/com/byritium/persistence/convertor/GroupConvertor.java
@@ -9,6 +9,21 @@ import com.byritium.types.constance.ObjectState;
 import java.time.LocalDateTime;
 
 public class GroupConvertor {
+    public static Group convertToGroup(GroupPo po) {
+        if (po == null) {
+            return null;
+        }
+        return Group.builder()
+                .groupName(po.getName())
+                .accountId(po.getAccountId())
+                .createTime(po.getCreateTime())
+                .createIdentifier(po.getCreatorIdentifier())
+                .os(po.getOs())
+                .build();
+
+    }
+
+
     public static GroupPo convertorGroupPo(Group group) {
         GroupPo groupPo = new GroupPo();
         groupPo.setName(group.getGroupName());
@@ -19,8 +34,12 @@ public class GroupConvertor {
         return groupPo;
     }
 
-    public static GroupMemberPo convertorGroupMemberPo(GroupMember groupMember){
+    public static GroupMemberPo convertorGroupMemberPo(GroupMember groupMember) {
         GroupMemberPo po = new GroupMemberPo();
+        po.setAccountId(groupMember.getAccountId());
+        po.setGroupId(groupMember.getGroupId());
+        po.setCreateTime(groupMember.getCreateTime());
+        po.setOs(groupMember.getOs());
         return po;
     }
 }
